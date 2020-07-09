248415af2fa56fd18379131604f292bbac262b45
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-07-09 14:28:48 -0400

    Update background.js

--- extension files/background.js
+++ extension files/background.js
@@ -6,8 +6,8 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   
 async function sendData2Sheets(jsonp,sender){
   var api_obj = await githubProfileObjectAPI(jsonp,sender);
-    console.log(['api',api_obj,encodeURIComponent(JSON.stringify(api_obj)).length])
-  var url = `https://script.google.com/macros/s/AKfycbzHHyUeIxQF_5Fxo_yG1Rh9_yfvvpB4nbu8LpVA37w/dev?obj=${encodeURIComponent(JSON.stringify(api_obj))}`
+  var your_web_app_url = 'https://script.google.com/macros/s/AKfycbzHHyUeIxQF_5678598_svpB4nbu8LpVA37w/dev';
+  var url = `${your_web_app_url}?obj=${encodeURIComponent(JSON.stringify(api_obj))}`
   var res = await fetch(url);
   var text = await res.text();
   console.log(text);
